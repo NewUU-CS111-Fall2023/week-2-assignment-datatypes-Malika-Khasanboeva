a30e08c4fbbd2ebb1390b0fd579c4eafd7131eea
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2023-10-25 22:53:51 +0500

    Update main.cpp
    added second problem

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,18 +4,18 @@
  */
 
 #include <iostream>
-#include "task_1.h"
+#include "task_2.h"
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    // call for task 1
+    // python file
     std::cout << "Task 2" << std::endl;
-    // call for task 2
+    main();
     std::cout << "Task 3" << std::endl;
-    // call for task 3
+    // python file
     std::cout << "Task 4" << std::endl;
-    // call for task 4
+    // python file
     std::cout << "Task 5" << std::endl;
-    // call for task 5
+    // not solved
     return 0;
 }
